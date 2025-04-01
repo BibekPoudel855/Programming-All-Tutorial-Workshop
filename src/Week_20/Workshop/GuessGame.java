f4b1b79d07dfff47f5f152a84f9d9d43222109e8
BibekPoudel855 <bibekpoudel855@gmail.com>
2025-04-01 10:05:57 +0545

    completion of week 20

--- src/Week_20/Workshop/GuessGame.java
+++ src/Week_20/Workshop/GuessGame.java
@@ -1,4 +1,36 @@
 package Week_20.Workshop;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GuessGame {
+    JFrame container;
+
+    public void makeFrame(){
+        container = new JFrame();
+        container.setLayout(new GridLayout());
+        container.setTitle("Guess Game");
+        container.setSize(500, 500);
+        container.setVisible(true);
+        container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addButton();
+    }
+    public void addButton(){
+        int randomNumber = (int) (Math.random() * 9);
+        System.out.println(randomNumber);
+        for(int i =1; i<=10; i++){
+            JButton button = new JButton(""+i);
+            container.add(button);
+            button.addActionListener(e -> {
+                JOptionPane.showMessageDialog(container, "Guess Correct");
+                if (button.getText().equals(String.valueOf(randomNumber))) {
+                    JOptionPane.showMessageDialog(container, "You have guessed it");
+                }
+            });
+        }
+    }
+    public static void main(String[] args) {
+        GuessGame g = new GuessGame();
+        g.makeFrame();
+    }
 }
---
+++ src/Week_21/Week_21_Assignment.java
@@ -0,0 +1,5 @@
+package Week_21;
+
+public class Week_21_Assignment {
+
+}
